b50173eff9a6fb49227796ff0534daadb75fc429
Khalid Hafid <khalitko@gmail.com>
2018-11-22 18:02:20 -0500

    fixing things with favorites list..still needs work

--- app/src/main/java/com/example/cst2335_finalproject/cst2335_final_project/FoodFragment.java
+++ app/src/main/java/com/example/cst2335_finalproject/cst2335_final_project/FoodFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -13,6 +14,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -20,7 +24,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 
 public class FoodFragment extends Fragment {
@@ -29,42 +33,42 @@ public class FoodFragment extends Fragment {
     SQLiteDatabase db;
     FoodActivity parent;
     Button delete;
+    Toolbar toolbar;
+
+
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         Bundle infoToPass = getArguments();
+//        super.onCreate(savedInstanceState);
 
         dbHelper = new FoodDatabaseHelper(getActivity());
         db = dbHelper.getWritableDatabase();
         parent = new FoodActivity();
         View view = inflater.inflate(R.layout.fragment_food,container,false);
 
-        Toolbar toolbar = view.findViewById(R.id.toolbar); //set up toolbar
-        if (this.getActivity().getActionBar() != null) this.getActivity().getActionBar().setDisplayShowTitleEnabled(true);
-        setHasOptionsMenu(true); //needed to make option menus to appear
+        toolbar = (Toolbar) view.findViewById(R.id.toolbar); //set up toolbar
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+//        if (this.getActivity().getActionBar() != null) this.getActivity().getActionBar().setDisplayShowTitleEnabled(false);
+//        setHasOptionsMenu(false); //needed to make option menus to appear
+
+
 
         Cursor foods = db.rawQuery("select * from " + FoodDatabaseHelper.TABLE_NAME, null);
         ListView favList= (ListView)view.findViewById(R.id.favList);
 
 
-        String[] columns = new String[] { FoodDatabaseHelper.KEY_LABEL, FoodDatabaseHelper.KEY_CALORIES };
+        String[] columns = new String[] { FoodDatabaseHelper.KEY_LABEL, FoodDatabaseHelper.KEY_CALORIES, FoodDatabaseHelper.KEY_FAT, FoodDatabaseHelper.KEY_CARBS };
         // THE XML DEFINED VIEWS WHICH THE DATA WILL BE BOUND TO
-        int[] to = new int[] { R.id.foodLabel, R.id.nutrition };
+        int[] to = new int[] { R.id.foodLabel, R.id.caloriesV, R.id.fatV, R.id.carbsV };
         favList.setAdapter(new SimpleCursorAdapter(getActivity(), R.layout.food_info, foods, columns, to));
 
-
-//        favList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//            @Override
-//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//                //if you want to delete
-//                db.delete(FoodDatabaseHelper.TABLE_NAME, FoodDatabaseHelper.KEY_ID + "=?", new String[] {Long.toString(id)});
-//                Toast.makeText(getActivity(), "Deleted id:" + id, Toast.LENGTH_SHORT);
-//                Cursor foods = db.rawQuery("select * from " + FoodDatabaseHelper.TABLE_NAME, null);
-//                favList.setAdapter(new SimpleCursorAdapter(getActivity(), R.layout.food_info, foods, columns, to));
-//                return false;
-//            }
-//        });
 
         favList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -95,5 +99,44 @@ public class FoodFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_favorite) {
+            Snackbar.make(getActivity().findViewById(android.R.id.content),R.string.favoritesList, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            return true;
+        }
+        if (id == R.id.search) {
+            Intent intent = new Intent(getActivity(), FoodActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.action_help) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(getString(R.string.Author) + "\n" + "\n" + getString(R.string.HowTo)).setTitle(R.string.Help).setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                }
+            }).show();
+            return true;
+        }
+        else if (id == R.id.action_settings) {
+            Toast.makeText(getActivity(), "Settings clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
