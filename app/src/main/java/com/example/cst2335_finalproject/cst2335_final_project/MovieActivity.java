115ad436ed6c28fcb0fb45cdb0608d118483acc6
dylanMcC <dylan.t.mccarthy@gmail.com>
2018-11-27 18:37:06 -0500

    Movies_Layout

--- app/src/main/java/com/example/cst2335_finalproject/cst2335_final_project/MovieActivity.java
+++ app/src/main/java/com/example/cst2335_finalproject/cst2335_final_project/MovieActivity.java
@@ -3,7 +3,8 @@ package com.example.cst2335_finalproject.cst2335_final_project;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MovieActivity extends Activity {
+public class
+MovieActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
