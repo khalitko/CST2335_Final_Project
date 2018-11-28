58683498d523866595bc335b48e7e486f3b395fa
dylanMcC <dylan.t.mccarthy@gmail.com>
2018-11-28 13:13:43 -0500

    Version 0.2.1
    -> Added Icons As well as Toolbar

--- app/src/main/java/com/example/cst2335_finalproject/cst2335_final_project/MovieActivity.java
+++ app/src/main/java/com/example/cst2335_finalproject/cst2335_final_project/MovieActivity.java
@@ -2,13 +2,25 @@ package com.example.cst2335_finalproject.cst2335_final_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
-public class
-MovieActivity extends Activity {
+public class MovieActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        Toolbar movieBar = findViewById(R.id.movieBar);
+        setSupportActionBar(movieBar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.movie_menu, m);
+        return true;
     }
 }
